a215790a1fd5d540afa6925285f5cdbaa978e331
AntonDrikITSM <bragimbragim@gmail.com>
2019-12-18 16:04:36 +0300

    prepared for deploy

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express           = require('express');
+const path              = require('path');
+let http                = require('http');
+let socketIO            = require('socket.io');
 
 const app = express();
+let server = http.Server(app);
+let io = socketIO(server);
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/chat'));
@@ -14,3 +18,22 @@ app.get('/*', function(req,res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('message', (data) => {
+    console.log('[server](message): %s', JSON.stringify(data));
+    // if (!data.action) {
+    //   console.log('pushed');
+    //   Message.create({
+    //     nick: data.nick,
+    //     text: data.text,
+    //     date: new Date()
+    //   })
+    // }
+    io.emit('message', data);
+  });
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
